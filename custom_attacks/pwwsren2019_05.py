823ffc0372d91e242ad26e090024ebf5a17e235e
Jens Hauser <jenshauser1@gmail.com>
2023-02-19 23:08:09 +0100

    Custom Attacks, filter words and indices to select from yelp dataset

---
+++ custom_attacks/pwwsren2019_05.py
@@ -0,0 +1,48 @@
+"""
+
+PWWS
+=======
+
+(Generating Natural Language Adversarial Examples through Probability Weighted Word Saliency)
+
+"""
+from textattack.attack_recipes.attack_recipe import AttackRecipe
+from textattack.constraints.pre_transformation import (
+    RepeatModification,
+    StopwordModification,
+)
+from textattack.constraints.semantics import WordEmbeddingDistance
+from textattack.constraints.semantics.sentence_encoders import UniversalSentenceEncoder
+from textattack.goal_functions import UntargetedClassification
+from textattack.search_methods import GreedyWordSwapWIR
+from textattack.shared.attack import Attack
+from textattack.transformations import WordSwapWordNet
+
+
+class PWWSRen2019_05(AttackRecipe):
+    """An implementation of Probability Weighted Word Saliency from "Generating
+    Natural Langauge Adversarial Examples through Probability Weighted Word
+    Saliency", Ren et al., 2019.
+
+    Words are prioritized for a synonym-swap transformation based on
+    a combination of their saliency score and maximum word-swap effectiveness.
+    Note that this implementation does not include the Named
+    Entity adversarial swap from the original paper, because it requires
+    access to the full dataset and ground truth labels in advance.
+
+    https://www.aclweb.org/anthology/P19-1103/
+    """
+
+    @staticmethod
+    def build(model):
+        transformation = WordSwapWordNet()
+        constraints = [RepeatModification(), StopwordModification()]
+
+        constraints.append(
+            WordEmbeddingDistance(min_cos_sim=0.5, include_unknown_words=False)
+        )
+
+        goal_function = UntargetedClassification(model)
+        # search over words based on a combination of their saliency score, and how efficient the WordSwap transform is
+        search_method = GreedyWordSwapWIR("weighted-saliency")
+        return Attack(goal_function, constraints, transformation, search_method)
---
+++ custom_attacks/pwwsren2019_075.py
@@ -0,0 +1,56 @@
+"""
+
+PWWS
+=======
+
+(Generating Natural Language Adversarial Examples through Probability Weighted Word Saliency)
+
+"""
+from textattack.attack_recipes.attack_recipe import AttackRecipe
+from textattack.constraints.pre_transformation import (
+    RepeatModification,
+    StopwordModification,
+)
+from textattack.constraints.semantics import WordEmbeddingDistance
+from textattack.constraints.semantics.sentence_encoders import UniversalSentenceEncoder
+from textattack.goal_functions import UntargetedClassification
+from textattack.search_methods import GreedyWordSwapWIR
+from textattack.shared.attack import Attack
+from textattack.transformations import WordSwapWordNet
+
+
+class PWWSRen2019_075(AttackRecipe):
+    """An implementation of Probability Weighted Word Saliency from "Generating
+    Natural Langauge Adversarial Examples through Probability Weighted Word
+    Saliency", Ren et al., 2019.
+
+    Words are prioritized for a synonym-swap transformation based on
+    a combination of their saliency score and maximum word-swap effectiveness.
+    Note that this implementation does not include the Named
+    Entity adversarial swap from the original paper, because it requires
+    access to the full dataset and ground truth labels in advance.
+
+    https://www.aclweb.org/anthology/P19-1103/
+    """
+
+    @staticmethod
+    def build(model):
+        transformation = WordSwapWordNet()
+        constraints = [RepeatModification(), StopwordModification()]
+
+        ##additional constraint on word embedding distance
+        use_constraint = UniversalSentenceEncoder(
+            threshold=0.9,  ##0.840845057,
+            metric="angular",
+            compare_against_original=True,  ##False
+            window_size=11,  ##15
+            skip_text_shorter_than_window=True,
+        )
+        constraints.append(use_constraint)
+
+        # constraints.append(WordEmbeddingDistance(min_cos_sim=0.75, include_unknown_words=False))
+
+        goal_function = UntargetedClassification(model)
+        # search over words based on a combination of their saliency score, and how efficient the WordSwap transform is
+        search_method = GreedyWordSwapWIR("weighted-saliency")
+        return Attack(goal_function, constraints, transformation, search_method)
