cdd98376cd8db41dc1ad31c0b6974ccd30d714e4
Jens Hauser <jenshauser1@gmail.com>
2023-11-12 16:10:21 +0100

    refactor collect_adv_dataset.py

--- collect_adv_dataset.py
+++ collect_adv_dataset.py
@@ -23,22 +23,8 @@ from textattack.attack_recipes.pwws_ren_2019 import PWWSRen2019
 from textattack.attack_recipes.textfooler_jin_2019 import TextFoolerJin2019
 from textattack.attack_results import FailedAttackResult, SkippedAttackResult
 from textattack.models.wrappers import HuggingFaceModelWrapper
-from tqdm import tqdm
-from transformers import BertForSequenceClassification as BertForSequenceClassification
-from transformers import BertTokenizer as BertTokenizer
-
-
-def write_json(new_data, filename="sample.json"):
-    with open(filename, "r+") as file:
-        # First we load existing data into a dict.
-        file_data = json.load(file)
-        # Join new_data with file_data inside emp_details
-        file_data["adv_examples"].append(new_data)
-        # Sets file's current position at offset.
-        file.seek(0)
-        # convert back to json.
-        json.dump(file_data, file, indent=4)
-
+from transformers import BertForSequenceClassification, BertTokenizer
+random.seed(0)
 
 attacker = {
     "BERTAttackLi2020": BERTAttackLi2020,
@@ -55,7 +41,6 @@ attacker = {
 }
 
 parser = argparse.ArgumentParser()
-random.seed(0)
 
 data_args = parser.add_argument_group("attack")
 data_args.add_argument("--dataset", type=str, default="ag_news")
@@ -71,191 +56,219 @@ data_args.add_argument(
     "--outfile", type=str, default="textfooler_adv_dataset_test_linear"
 )
 
-args = parser.parse_args()
+def write_json(new_data, filename="sample.json"):
+    """
+    Appends new data to a JSON file.
 
-## writing csv file with adversarial examples. format: original text, perturbed text, original label
-out_file = f"adv_datasets/{args.outfile}.json"
+    Args:
+    - new_data (dict): Data to append.
+    - filename (str, optional): File to append the data to. Defaults to "sample.json".
+    """
+    with open(filename, "r+") as file:
+        file_data = json.load(file)
+        file_data["adv_examples"].append(new_data)
+        file.seek(0)
+        json.dump(file_data, file, indent=4)
 
-# set number of classes according to dataset
-if args.dataset == "ag_news":
-    n_classes = 4
-elif args.dataset == "imdb" or args.dataset == "yelp_polarity":
-    n_classes = 2
+def load_model_and_tokenizer(pre_trained_model, max_length, fine_tuned_model, device, num_labels):
+    """
+    Loads the BERT model and tokenizer.
 
-## load model and tokenizer
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-tokenizer = BertTokenizer.from_pretrained(
-    args.pre_trained_model, model_max_length=args.max_length, do_lower_case=True
-)
-model = BertForSequenceClassification.from_pretrained(
-    args.pre_trained_model, num_labels=n_classes
-).to(device)
-model.load_state_dict(torch.load(f"models/{args.fine_tuned_model}.pth"))
-model_wrapper = HuggingFaceModelWrapper(model, tokenizer)
+    Args:
+    - pre_trained_model (str): Name of the pre-trained model.
+    - max_length (int): Maximum length of the tokens.
+    - fine_tuned_model (str): Path to the fine-tuned model.
+    - device (torch.device): Device to load the model on (CPU or GPU).
+    - num_labels (int): Number of labels in the classification task.
 
-## load dataset
-test_dataset = load_dataset(args.dataset, split="test")
-if args.dataset == "yelp_polarity":
-    ## we only keep text documents shorter 80 words, to keep it simpler for human evaluations
-    with open("other/yelp_polarity_indices_shorter_80.json", "r") as openfile:
-        indices = json.load(openfile)
-    test_dataset = test_dataset.select(indices["indices_shorter_80"])
+    Returns:
+    - tuple: A tuple containing the loaded model and tokenizer.
+    """
+    tokenizer = BertTokenizer.from_pretrained(pre_trained_model, model_max_length=max_length, do_lower_case=True)
+    model = BertForSequenceClassification.from_pretrained(pre_trained_model, num_labels=num_labels).to(device)
+    model.load_state_dict(torch.load(f"models/{fine_tuned_model}.pth"))
 
-## we select 1000 samples
-selected = random.sample(range(len(test_dataset)), 1000)
-test_dataset = test_dataset.select(selected)
+    return model, tokenizer
 
-custom_dataset = []
-for sample in test_dataset:
-    custom_dataset.append((sample["text"], sample["label"]))
+def load_dataset_and_filter(args):
+    """
+    Loads the specified dataset and applies filtering based on the dataset type.
 
-## create attack
-attack = attacker[args.attacker].build(model_wrapper)
+    Args:
+    - args (argparse.Namespace): Arguments containing dataset specifications.
 
-## attack samples
-results_iterable = attack.attack_dataset(custom_dataset)
-perturbed_word_percentages = np.zeros(len(custom_dataset))
-words_changed = np.zeros(len(custom_dataset))
-failed_attacks = 0
-skipped_attacks = 0
-successful_attacks = 0
-max_words_changed = 0
+    Returns:
+    - datasets.arrow_dataset.Dataset: The loaded and filtered dataset.
+    """
+    test_dataset = load_dataset(args.dataset, split="test")
+    if args.dataset == "yelp_polarity":
+        with open("other/yelp_polarity_indices_shorter_80.json", "r") as openfile:
+            indices = json.load(openfile)
+        test_dataset = test_dataset.select(indices["indices_shorter_80"])
 
-total_attacks = len(custom_dataset)
-with tqdm(total=len(custom_dataset)) as progress_bar:
+    selected = random.sample(range(len(test_dataset)), 1000)
+    return test_dataset.select(selected)
+
+def prepare_attack(args, model_wrapper):
+    """
+    Prepares the attack based on the provided arguments.
+
+    Args:
+    - args (argparse.Namespace): Arguments containing attack specifications.
+    - model_wrapper (textattack.models.wrappers.ModelWrapper): The model wrapper used for attacks.
+
+    Returns:
+    - textattack.attack_recipes.AttackRecipe: The configured attack recipe.
+    """
+    attack = attacker[args.attacker].build(model_wrapper)
+    return attack
+
+def attack_samples(test_dataset, attack):
+    """
+    Attacks the samples in the dataset using the specified attack method.
+
+    Args:
+    - test_dataset (datasets.arrow_dataset.Dataset): The dataset to be attacked.
+    - attack (textattack.attack_recipes.AttackRecipe): The attack method to use.
+
+    Returns:
+    - list: A list containing the results of the attack on each sample.
+    """
+    custom_dataset = [(sample["text"], sample["label"]) for sample in test_dataset]
+    results_iterable = attack.attack_dataset(custom_dataset)
+
+    return results_iterable
+
+def process_attack_results(results_iterable, custom_dataset, out_file):
+    """
+    Processes the results of the attack, collecting data, statistics, and writing successful attacks to a JSON file.
+
+    Args:
+    - results_iterable (iterable): Iterable object containing attack results.
+    - custom_dataset (list): List of tuples containing the original texts and labels.
+    - out_file (str): Path to the output JSON file.
+
+    Returns:
+    - dict: A dictionary containing summary statistics of the attack results.
+    """
+    failed_attacks, skipped_attacks, successful_attacks = 0, 0, 0
+    words_changed = []
+    perturbed_word_percentages = []
+
     for i, result in enumerate(results_iterable):
-        # print(result.__str__(color_method='ansi'))
-        if successful_attacks + failed_attacks != 0 and i % 50 == 0:
-            print(i, successful_attacks / (successful_attacks + failed_attacks))
-            print(result.__str__(color_method="ansi"))
         original_text, label = custom_dataset[i]
         if isinstance(result, FailedAttackResult):
             failed_attacks += 1
-            progress_bar.update(1)
-            continue
         elif isinstance(result, SkippedAttackResult):
             skipped_attacks += 1
-            progress_bar.update(1)
-            continue
-        successful_attacks += 1
-
-        ## collect all important data and store in json file
-        changed_indices_original = result.original_result.attacked_text.all_words_diff(
-            result.perturbed_result.attacked_text
-        )
-        changed_indices_perturbed = (
-            result.perturbed_result.attacked_text.all_words_diff(
-                result.original_result.attacked_text
-            )
-        )
-        if successful_attacks == 1:
-            data = {
-                "adv_examples": [
-                    {
-                        "color_string": result.__str__(color_method="ansi"),
-                        "original_text": result.original_text(),
-                        "perturbed_text": result.perturbed_text(),
-                        "original_words": result.original_result.attacked_text.words,
-                        "perturbed_words": result.perturbed_result.attacked_text.words,
-                        "changed_idx_orig": list(changed_indices_original),
-                        "changed_idx_pert": list(changed_indices_perturbed),
-                        "label": custom_dataset[i][-1],
-                        "output_orig": [
-                            np.argmax(result.original_result.raw_output).item(),
-                            max(result.original_result.raw_output).item(),
-                        ],
-                        "output_pert": [
-                            np.argmax(result.perturbed_result.raw_output).item(),
-                            max(result.perturbed_result.raw_output).item(),
-                        ],
-                    }
-                ]
-            }
-            json_object = json.dumps(data, indent=4)
-
-            with open(out_file, "w") as outfile:
-                outfile.write(json_object)
-
         else:
-            new_data = {
-                "color_string": result.__str__(color_method="ansi"),
-                "original_text": result.original_text(),
-                "perturbed_text": result.perturbed_text(),
-                "original_words": result.original_result.attacked_text.words,
-                "perturbed_words": result.perturbed_result.attacked_text.words,
-                "changed_idx_orig": list(changed_indices_original),
-                "changed_idx_pert": list(changed_indices_perturbed),
-                "label": custom_dataset[i][-1],
-                "output_orig": [
-                    np.argmax(result.original_result.raw_output).item(),
-                    max(result.original_result.raw_output).item(),
-                ],
-                "output_pert": [
-                    np.argmax(result.perturbed_result.raw_output).item(),
-                    max(result.perturbed_result.raw_output).item(),
-                ],
-            }
-            try:
-                write_json(new_data, filename=out_file)
-            except:
-                print("entered except")
-                print(new_data)
-                time.sleep(1)
-                write_json(new_data, filename=out_file)
+            successful_attacks += 1
+            attack_data = extract_attack_data(result)  # Function to extract relevant data from result
+            append_to_json(attack_data, out_file)
 
-        num_words_changed = len(
-            result.original_result.attacked_text.all_words_diff(
-                result.perturbed_result.attacked_text
-            )
+    # Original classifier success rate on these samples.
+    total_attacks = failed_attacks + skipped_attacks + successful_attacks
+    original_accuracy = (total_attacks - skipped_attacks) * 100.0 / (total_attacks)
+    original_accuracy = str(round(original_accuracy, 2)) + "%"
+
+    # New classifier success rate on these samples.
+    accuracy_under_attack = (failed_attacks) * 100.0 / (total_attacks)
+    accuracy_under_attack = str(round(accuracy_under_attack, 2)) + "%"
+
+    # Attack success rate.
+    if successful_attacks + failed_attacks == 0:
+        attack_success_rate = 0
+    else:
+        attack_success_rate = (
+            successful_attacks * 100.0 / (successful_attacks + failed_attacks)
         )
-        words_changed[i] = num_words_changed
-        max_words_changed = max(
-            max_words_changed or num_words_changed, num_words_changed
-        )
-        if len(result.original_result.attacked_text.words) > 0:
-            perturbed_word_percentage = (
-                num_words_changed
-                * 100.0
-                / len(result.original_result.attacked_text.words)
-            )
-        else:
-            perturbed_word_percentage = 0
-        perturbed_word_percentages[i] = perturbed_word_percentage
-        progress_bar.update(1)
+        attack_success_rate = str(round(attack_success_rate, 2)) + "%"
 
-# Original classifier success rate on these samples.
-original_accuracy = (total_attacks - skipped_attacks) * 100.0 / (total_attacks)
-original_accuracy = str(round(original_accuracy, 2)) + "%"
+    perturbed_word_percentages = perturbed_word_percentages[perturbed_word_percentages > 0]
+    average_perc_words_perturbed = perturbed_word_percentages.mean() if perturbed_word_percentages else 0
+    average_perc_words_perturbed = str(round(average_perc_words_perturbed, 2)) + "%"
 
-# New classifier success rate on these samples.
-accuracy_under_attack = (failed_attacks) * 100.0 / (total_attacks)
-accuracy_under_attack = str(round(accuracy_under_attack, 2)) + "%"
+    words_changed = words_changed[words_changed > 0]
+    average_words_changed = words_changed.mean() if words_changed else 0
+    average_words_changed = round(average_words_changed, 2)
 
-# Attack success rate.
-if successful_attacks + failed_attacks == 0:
-    attack_success_rate = 0
-else:
-    attack_success_rate = (
-        successful_attacks * 100.0 / (successful_attacks + failed_attacks)
+    return {
+        "failed_attacks": failed_attacks,
+        "skipped_attacks": skipped_attacks,
+        "successful_attacks": successful_attacks,
+        "original_accuracy": original_accuracy,
+        "accuracy_under_attack": accuracy_under_attack,
+        "attack_success_rate": attack_success_rate,
+        "average_words_changed": average_words_changed,
+        "average_perturbed_percentage": average_perc_words_perturbed
+    }
+
+
+def append_to_json(data, filename):
+    """
+    Appends data to a JSON file.
+
+    Args:
+    - data (dict): Data to append to the file.
+    - filename (str): Path to the JSON file.
+    """
+    try:
+        with open(filename, "r+") as file:
+            file_data = json.load(file)
+            file_data["adv_examples"].append(data)
+            file.seek(0)
+            json.dump(file_data, file, indent=4)
+    except FileNotFoundError:
+        with open(filename, "w") as file:
+            json.dump({"adv_examples": [data]}, file, indent=4)
+
+def extract_attack_data(result):
+    """
+    Extracts relevant data from a successful attack result.
+
+    Args:
+    - result (textattack.attack_results.SuccessfulAttackResult): The result of a successful attack.
+
+    Returns:
+    - dict: A dictionary containing extracted data from the attack result.
+    """
+    changed_indices_original = result.original_result.attacked_text.all_words_diff(
+        result.perturbed_result.attacked_text
     )
-    attack_success_rate = str(round(attack_success_rate, 2)) + "%"
+    changed_indices_perturbed = result.perturbed_result.attacked_text.all_words_diff(
+        result.original_result.attacked_text
+    )
 
-perturbed_word_percentages = perturbed_word_percentages[perturbed_word_percentages > 0]
-average_perc_words_perturbed = perturbed_word_percentages.mean()
-average_perc_words_perturbed = str(round(average_perc_words_perturbed, 2)) + "%"
+    return {
+        "color_string": result.__str__(color_method="ansi"),
+        "original_text": result.original_text(),
+        "perturbed_text": result.perturbed_text(),
+        "original_words": result.original_result.attacked_text.words,
+        "perturbed_words": result.perturbed_result.attacked_text.words,
+        "changed_idx_orig": list(changed_indices_original),
+        "changed_idx_pert": list(changed_indices_perturbed),
+        "label": result.original_result.output,
+        "output_orig": [
+            np.argmax(result.original_result.raw_output).item(),
+            max(result.original_result.raw_output).item(),
+        ],
+        "output_pert": [
+            np.argmax(result.perturbed_result.raw_output).item(),
+            max(result.perturbed_result.raw_output).item(),
+        ],
+    }
 
-words_changed = words_changed[words_changed > 0]
-average_words_changed = words_changed.mean()
-average_words_changed = str(round(average_words_changed, 2)) + "%"
+if __name__ == "__main__":
+    args = parser.parse_args()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    out_file = f"adv_datasets/{args.outfile}.json"
 
-summary_table_rows = [
-    ["Number of successful attacks:", str(successful_attacks)],
-    ["Number of failed attacks:", str(failed_attacks)],
-    ["Number of skipped attacks:", str(skipped_attacks)],
-    ["Original accuracy:", original_accuracy],
-    ["Accuracy under attack:", accuracy_under_attack],
-    ["Attack success rate:", attack_success_rate],
-    ["Average perturbed word %:", average_perc_words_perturbed],
-    ["Average num. words changed:", average_words_changed],
-]
-print(summary_table_rows)
+    n_classes = 4 if args.dataset == "ag_news" else 2
+    model, tokenizer = load_model_and_tokenizer(
+        args.pre_trained_model, args.max_length, args.fine_tuned_model, device, n_classes
+    )
+    model_wrapper = HuggingFaceModelWrapper(model, tokenizer)
+    test_dataset = load_dataset_and_filter(args)
+    attack = prepare_attack(args, model_wrapper)
+    attack_results = attack_samples(test_dataset, attack)
+    print(process_attack_results(attack_results, test_dataset, out_file))
