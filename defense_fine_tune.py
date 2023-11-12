1035fe4f84fee0fc040cf8e2cf52fac26289ddad
Jens Hauser <jenshauser1@gmail.com>
2023-11-12 20:24:56 +0100

    refactor defense_fine_tune.py

--- defense_fine_tune.py
+++ defense_fine_tune.py
@@ -5,7 +5,6 @@ import random
 import time
 
 import torch
-from helper_functions import get_perturbed_batch, prepare_train_and_test_data
 from nltk.corpus import stopwords
 from textattack.shared import WordEmbedding
 from torch.utils.data import DataLoader
@@ -16,122 +15,204 @@ from transformers import (
     get_linear_schedule_with_warmup,
 )
 
-random.seed(0)
+from helper_functions import get_perturbed_batch, prepare_train_and_test_data
 
-parser = argparse.ArgumentParser()
-data_args = parser.add_argument_group("dataset")
-data_args.add_argument("--dataset", type=str, default="ag_news")
-data_args.add_argument("--pre_trained_model", type=str, default="bert-base-uncased")
-data_args.add_argument("--batch_size", type=int, default=32)
-data_args.add_argument("--n_epochs", type=int, default=12)
-data_args.add_argument("--max_length", type=int, default=128)
-data_args.add_argument("--probas", type=str, default="linear")
-data_args.add_argument("--replace_rate", type=float, default=0.4)
-data_args.add_argument("--cos_sim", type=float, default=0.5)
-data_args.add_argument("--stop_words", type=str, default="all")
-args = parser.parse_args()
+# Setup parser for command line arguments
+def setup_parser():
+    """
+    Sets up the argument parser with necessary arguments for dataset and training parameters.
 
-# Load stopwords
-with open("other/filter_words.txt", "r") as file:
-    filter_words = json.load(file)
+    Returns:
+    - argparse.ArgumentParser: Configured argument parser.
+    """
+    parser = argparse.ArgumentParser()
+    data_args = parser.add_argument_group("dataset")
+    data_args.add_argument("--dataset", type=str, default="ag_news")
+    data_args.add_argument("--pre_trained_model", type=str, default="bert-base-uncased")
+    data_args.add_argument("--batch_size", type=int, default=32)
+    data_args.add_argument("--n_epochs", type=int, default=12)
+    data_args.add_argument("--max_length", type=int, default=128)
+    data_args.add_argument("--probas", type=str, default="linear")
+    data_args.add_argument("--replace_rate", type=float, default=0.4)
+    data_args.add_argument("--cos_sim", type=float, default=0.5)
+    data_args.add_argument("--stop_words", type=str, default="all")
+    return parser
 
-if args.stop_words == "textfooler":
-    filter_words = set(filter_words)
-else:
-    filter_words = set(filter_words).union(stopwords.words("english"))
 
-# Load counterfitted word vectors
-embedding = WordEmbedding.counterfitted_GLOVE_embedding()
+def load_filter_words(args):
+    """
+    Loads a set of filter words based on specified stopwords strategy.
 
-# Load tokenizer and model
-tokenizer = BertTokenizer.from_pretrained(
-    args.pre_trained_model, model_max_length=args.max_length, do_lower_case=True
-)
+    Args:
+    - args (argparse.Namespace): Parsed command line arguments.
 
-if args.dataset == "ag_news":
-    n_classes = 4
-elif args.dataset == "imdb" or args.dataset == "yelp_polarity":
-    n_classes = 2
+    Returns:
+    - set: A set of filter words.
+    """
+    # Load stopwords
+    with open("other/filter_words.txt", "r") as file:
+        filter_words = json.load(file)
 
-model = BertForSequenceClassification.from_pretrained(
-    args.pre_trained_model, num_labels=n_classes
-)
+    if args.stop_words == "textfooler":
+        filter_words = set(filter_words)
+    else:
+        filter_words = set(filter_words).union(stopwords.words("english"))
 
-# Load and prepare datasets
-train_dataset_encoded, test_dataset_encoded = prepare_train_and_test_data(
-    args.dataset, args.max_length, tokenizer
-)
-train_loader = DataLoader(
-    train_dataset_encoded, batch_size=args.batch_size, shuffle=True
-)
+    return filter_words
 
-# Parameters for training
-optim = AdamW(model.parameters(), lr=2e-5)
-scheduler = get_linear_schedule_with_warmup(
-    optim, len(train_loader) * 0.5, len(train_loader) * args.n_epochs
-)
 
-# Custom training loop
-s_time = time.time()
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-print("Finetuning using: ", device)
-model.to(device)
-model.train()
-for epoch in range(args.n_epochs):
-    counter = 0
-    for batch in train_loader:
-        optim.zero_grad()
-        input_ids = batch["input_ids"].to(device)
-        attention_mask = batch["attention_mask"].to(device)
-        token_type_ids = batch["token_type_ids"].to(device)
-        labels = batch["labels"].to(device)
-        input_ids_pert, token_type_ids_pert, attention_mask_pert = get_perturbed_batch(
-            input_ids,
-            token_type_ids,
-            attention_mask,
-            labels.reshape(-1),
-            args.replace_rate,
-            50,
-            args.cos_sim,
-            probas=args.probas,
-            filter_words=filter_words,
-            max_length=args.max_length,
-            model=model,
-            tokenizer=tokenizer,
-            embedding=embedding,
-            device=device,
-        )
-        input_ids = torch.cat([input_ids, input_ids_pert.to(device)])
-        attention_mask = torch.cat([attention_mask, attention_mask_pert.to(device)])
-        token_type_ids = torch.cat([token_type_ids, token_type_ids_pert.to(device)])
-        labels = torch.cat([labels, labels])
+def train_model(
+    args,
+    train_loader,
+    model,
+    device,
+    optim,
+    scheduler,
+    embedding,
+    tokenizer,
+    filter_words,
+):
+    """
+    Custom training loop for the model.
 
-        outputs = model(
-            input_ids,
-            token_type_ids=token_type_ids,
-            attention_mask=attention_mask,
-            labels=labels,
-        )
-        loss = outputs[0]
-        loss.backward()
-        optim.step()
-        scheduler.step()
-        counter += 1
-        if counter % 100 == 0:
-            print(
-                "Epoch  ",
-                epoch,
-                "progress: ",
-                counter * args.batch_size / (len(train_dataset_encoded)) * 100,
-                "%",
-            )
-            print("Loss: ", loss)
+    Args:
+    - args (argparse.Namespace): Parsed command line arguments.
+    - train_loader (torch.utils.data.DataLoader): DataLoader for the training dataset.
+    - model (torch.nn.Module): The BERT model for sequence classification.
+    - device (torch.device): Device to train the model on.
+    - optim (torch.optim.Optimizer): Optimizer for training.
+    - scheduler (torch.optim.lr_scheduler): Learning rate scheduler.
+    - embedding (textattack.shared.WordEmbedding): Pre-loaded word embeddings.
+    - tokenizer (transformers.BertTokenizer): Tokenizer for text processing.
+    - filter_words (set): Set of words to be filtered during training.
 
-    model.eval()
-    torch.save(
-        model.state_dict(),
-        f"models/{args.pre_trained_model}-{args.dataset.replace('_', '')}-ml{args.max_length}-rr{str(args.replace_rate).replace('.', '') + args.probas}-ct{str(args.cos_sim).replace('.', '')}-ep{str(epoch + 1) +'_' + str(args.n_epochs)}.pth",
-    )
+    Returns:
+    - torch.nn.Module: Trained model.
+    """
     model.train()
-end_time = time.time()
-print("Total time: ", str(datetime.timedelta(seconds=int(end_time - s_time))))
+    for epoch in range(args.n_epochs):
+        counter = 0
+        for batch in train_loader:
+            optim.zero_grad()
+            input_ids, attention_mask, token_type_ids, labels = (
+                batch["input_ids"].to(device),
+                batch["attention_mask"].to(device),
+                batch["token_type_ids"].to(device),
+                batch["labels"].to(device),
+            )
+            (
+                input_ids_pert,
+                token_type_ids_pert,
+                attention_mask_pert,
+            ) = get_perturbed_batch(
+                input_ids,
+                token_type_ids,
+                attention_mask,
+                labels.reshape(-1),
+                args.replace_rate,
+                50,
+                args.cos_sim,
+                probas=args.probas,
+                filter_words=filter_words,
+                max_length=args.max_length,
+                model=model,
+                tokenizer=tokenizer,
+                embedding=embedding,
+                device=device,
+            )
+            input_ids = torch.cat([input_ids, input_ids_pert.to(device)])
+            attention_mask = torch.cat([attention_mask, attention_mask_pert.to(device)])
+            token_type_ids = torch.cat([token_type_ids, token_type_ids_pert.to(device)])
+            labels = torch.cat([labels, labels])
+
+            outputs = model(
+                input_ids,
+                token_type_ids=token_type_ids,
+                attention_mask=attention_mask,
+                labels=labels,
+            )
+            loss = outputs[0]
+            loss.backward()
+            optim.step()
+            scheduler.step()
+            counter += 1
+            if counter % 100 == 0:
+                print(
+                    "Epoch",
+                    epoch,
+                    "progress:",
+                    counter * args.batch_size / len(train_loader.dataset) * 100,
+                    "%",
+                )
+                print("Loss:", loss.item())
+
+    return model
+
+
+def main():
+    args = setup_parser().parse_args()
+    random.seed(0)
+
+    # Start time
+    s_time = time.time()
+
+    # Load stopwords and embeddings
+    filter_words = load_filter_words(args)
+    embedding = WordEmbedding.counterfitted_GLOVE_embedding()
+
+    # Initialize tokenizer and model
+    tokenizer = BertTokenizer.from_pretrained(
+        args.pre_trained_model, model_max_length=args.max_length, do_lower_case=True
+    )
+    n_classes = 4 if args.dataset == "ag_news" else 2
+    model = BertForSequenceClassification.from_pretrained(
+        args.pre_trained_model, num_labels=n_classes
+    )
+
+    # Load and prepare datasets
+    train_dataset_encoded, _ = prepare_train_and_test_data(
+        args.dataset, args.max_length, tokenizer
+    )
+    train_loader = DataLoader(
+        train_dataset_encoded, batch_size=args.batch_size, shuffle=True
+    )
+
+    # Set up optimizer and scheduler
+    optim = AdamW(model.parameters(), lr=2e-5)
+    scheduler = get_linear_schedule_with_warmup(
+        optim, len(train_loader) * 0.5, len(train_loader) * args.n_epochs
+    )
+
+    # Training
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    print("Finetuning using:", device)
+    model.to(device)
+    trained_model = train_model(
+        args,
+        train_loader,
+        model,
+        device,
+        optim,
+        scheduler,
+        embedding,
+        tokenizer,
+        filter_words,
+    )
+
+    # Save final model
+    model_save_path = (
+        f"models/{args.pre_trained_model}-"
+        f"{args.dataset.replace('_', '')}-"
+        f"ml{args.max_length}-"
+        f"rr{str(args.replace_rate).replace('.', '') + args.probas}-"
+        f"ct{str(args.cos_sim).replace('.', '')}-"
+        f"ep{args.n_epochs}.pth"
+    )
+    torch.save(trained_model.state_dict(), model_save_path)
+
+    end_time = time.time()
+    print("Total time:", str(datetime.timedelta(seconds=int(end_time - s_time))))
+
+
+if __name__ == "__main__":
+    main()
